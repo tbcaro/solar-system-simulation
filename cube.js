82e868c1e900f1b214bc5c6c88a6f49d14494a3d
Travis Caro <travisbcaro@gmail.com>
2017-05-09 23:40:46 -0400

    Complete planet logic

--- cube.js
+++ cube.js
@@ -5,7 +5,7 @@ function SolarSystem () {
     */
     init: function () {
       // TBC : Constants
-      this.STAR_SIZE = 25;
+      this.STAR_SIZE = 50;
       this.STAR_COLOR = 0xffff00;
 
       // TBC : Setup threejs required entities
@@ -19,13 +19,14 @@ function SolarSystem () {
       this.controls.dampingFactor = 1;
       this.controls.enableZoom = true;
 
-      // TBC : Set renderer sizes and camera position
-      this.renderer.setSize( window.innerWidth, window.innerHeight );
-      this.camera.position.z = 200;
-
       // TBC : Add objects to scene
       this.seedStar();
       this.seedPlanets();
+
+      // TBC : Set renderer sizes and camera position
+      this.renderer.setSize( window.innerWidth, window.innerHeight );
+      this.camera.position.y = 200;
+      this.camera.lookAt( this.star.position );
 
       // TBC : Add render canvas to DOM
       document.body.appendChild( this.renderer.domElement );
@@ -41,17 +42,27 @@ function SolarSystem () {
       this.scene.add( this.star );
 
       // TBC : Also add light
-      this.scene.add( new THREE.AmbientLight( 0x707070 ) );
-      this.scene.add( new THREE.PointLight( this.STAR_COLOR, 1, 100 ) );
+      this.scene.add( new THREE.AmbientLight( 0xa0a0a0 ) );
+      this.scene.add( new THREE.PointLight( this.STAR_COLOR, 1 ) );
     },
     /**
     *
     */
     seedPlanets: function () {
-      const SPEED_MODIFIER = .25;
-      // this.planets = { mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto };
-      this.planets = { mercury: { } };
+      const SPEED_MODIFIER = .10;
+      this.planets = {
+        mercury: { },
+        venus: { },
+        earth: { },
+        mars: { },
+        jupiter: { },
+        saturn: { },
+        uranus: { },
+        neptune: { },
+        pluto: { }
+      };
 
+      var orbitOffset = this.STAR_SIZE;
 
       for ( var p in this.planets ) {
         var rotSpeed;
@@ -63,19 +74,94 @@ function SolarSystem () {
 
         switch (p) {
           case 'mercury':
-            rotSpeed = 1 / 58;
-            orbSpeed = 2 * Math.PI * SPEED_MODIFIER / 88;
-            orbRadius = this.STAR_SIZE + 10;
-            color = 0xe2e2e2;
             size = 1.5;
+            rotSpeed = 1 / 58 * SPEED_MODIFIER;
+            orbSpeed = 2 * Math.PI / 88 * SPEED_MODIFIER;
+            orbRadius = orbitOffset + size + size * 5;
+            orbitOffset = orbRadius;
+            color = 0xe2e2e2;
             spherical = 0;
             break;
 
-          // case 'venus':
-          //   break;
-          //
-          // case 'earth':
-          //   break;
+          case 'venus':
+            size = 3.7;
+            rotSpeed = 1 / 116 * SPEED_MODIFIER;
+            orbSpeed = 2 * Math.PI / 225 * SPEED_MODIFIER;
+            orbRadius = orbitOffset + size + size * 5;
+            orbitOffset = orbRadius;
+            color = 0xee39e1c;
+            spherical = 2;
+            break;
+
+          case 'earth':
+            size = 3.9;
+            rotSpeed = 1 * SPEED_MODIFIER;
+            orbSpeed = 2 * Math.PI / 365 * SPEED_MODIFIER;
+            orbRadius = orbitOffset + size + size * 5;
+            orbitOffset = orbRadius;
+            color = 0x6b93d6;
+            spherical = 2;
+            break;
+
+          case 'mars':
+            size = 2.1;
+            rotSpeed = 1 * SPEED_MODIFIER;
+            orbSpeed = 2 * Math.PI / 686 * SPEED_MODIFIER;
+            orbRadius = orbitOffset + size + size * 5;
+            orbitOffset = orbRadius;
+            color = 0xc1440e;
+            spherical = 0;
+            break;
+
+          case 'jupiter':
+            size = 20;
+            rotSpeed = 2.5 * SPEED_MODIFIER;
+            orbSpeed = 2 * Math.PI / 900 * SPEED_MODIFIER;
+            orbRadius = orbitOffset + size + size * 5;
+            orbitOffset = orbRadius;
+            color = 0xd8ca9d;
+            spherical = 5;
+            break;
+
+          case 'saturn':
+            size = 15;
+            rotSpeed = 2 * SPEED_MODIFIER;
+            orbSpeed = 2 * Math.PI / 1500 * SPEED_MODIFIER;
+            orbRadius = orbitOffset + size + size * 5;
+            orbitOffset = orbRadius;
+            color = 0xead6b8;
+            spherical = 5;
+            break;
+
+          case 'uranus':
+            size = 7;
+            rotSpeed = 1.5 * SPEED_MODIFIER;
+            orbSpeed = 2 * Math.PI / 2000 * SPEED_MODIFIER;
+            orbRadius = orbitOffset + size + size * 5;
+            orbitOffset = orbRadius;
+            color = 0xd1e7e7;
+            spherical = 5;
+            break;
+
+          case 'neptune':
+            size = 7;
+            rotSpeed = 1.5 * SPEED_MODIFIER;
+            orbSpeed = 2 * Math.PI / 2500 * SPEED_MODIFIER;
+            orbRadius = orbitOffset + size + size * 5;
+            orbitOffset = orbRadius;
+            color = 0x5b5ddf;
+            spherical = 5;
+            break;
+
+          case 'pluto':
+            size = 0.7;
+            rotSpeed = 1 / 7 * SPEED_MODIFIER;
+            orbSpeed = 2 * Math.PI / 3000 * SPEED_MODIFIER;
+            orbRadius = orbitOffset + size + size * 5;
+            orbitOffset = orbRadius;
+            color = 0x968570;
+            spherical = 0;
+            break;
         }
 
         var geometry = new THREE.IcosahedronGeometry( size, spherical );
